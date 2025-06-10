89ea9faca12d6950423b7759e63e00f1012fcc67
Mohammad Asif <asif@yellowblock.net>
2025-06-10 11:26:28 +0530

    added 3ds and delievery endpoints

---
+++ Services/Card/3DS.cs
@@ -0,0 +1,76 @@
+﻿using System.Transactions;
+
+namespace Wirex_POC.Services.Card
+{
+    public class _3DS
+    {
+        public static async Task GetActiveRequest(string accessToken)
+        {
+            string url = $"https://api-baas.wirexapp.tech/api/v1/cards/3ds/requests";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                    { "Authorization", accessToken },
+                    { "X-Chain-Id", "84532" },
+                    { "X-User-Email", "mohdasifreal@gmail.com" }
+                }
+            };
+
+            using var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+        public static async Task GetApproveRequest(string accessToken, string transactionId)
+        {
+            string url = $"https://api-baas.wirexapp.tech/api/v1/cards/3ds/requests/{transactionId}/approve";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                    { "Authorization", accessToken },
+                    { "X-Chain-Id", "84532" },
+                    { "X-User-Email", "mohdasifreal@gmail.com" }
+                }
+            };
+
+            using var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+        public static async Task GetDeclineRequest(string accessToken, string transactionId)
+        {
+            string url = $"https://api-baas.wirexapp.tech/api/v1/cards/3ds/requests/{transactionId}/decline";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                    { "Authorization", accessToken },
+                    { "X-Chain-Id", "84532" },
+                    { "X-User-Email", "mohdasifreal@gmail.com" }
+                }
+            };
+
+            using var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
---
+++ Services/Card/Delievery.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Wirex_POC.Services.Card
+{
+    public class Delievery
+    {
+        public static async Task GetDelieveryCountries(string accessToken)
+        {
+            string url = $"https://api-baas.wirexapp.tech/api/v1/cards/delivery/countries";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                    { "Authorization", accessToken },
+                    { "X-Chain-Id", "84532" },
+                    { "X-User-Email", "mohdasifreal@gmail.com" }
+                }
+            };
+
+            using var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+        public static async Task GetDelieveryMethod(string accessToken, string country)
+        {
+            string url = $"https://api-baas.wirexapp.tech/api/v1/cards/delivery/methods/{country}";
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url),
+                Headers =
+                {
+                    { "Accept", "application/json" },
+                    { "Authorization", accessToken },
+                    { "X-Chain-Id", "84532" },
+                    { "X-User-Email", "mohdasifreal@gmail.com" }
+                }
+            };
+
+            using var response = await client.SendAsync(request);
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+
+
+    }
+}
